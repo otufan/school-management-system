f1006e372f3962df78ba3baa5018402fa0537da9
otufan <omertufanca@gmail.com>
2024-01-02 16:06:36 +0100

    not working

--- Teacher_UI/teacher_main.py
+++ Teacher_UI/teacher_main.py
@@ -21,16 +21,12 @@ class Main_Window(QMainWindow, Ui_MainWindow):
         super(Main_Window,self).__init__()
         self.setupUi(self)
         self.setWindowTitle("Teacher Page")
-        print("Teacher window is opened")
 
         #User.set_currentuser("admin@example.com")
-        self.current_user_email = User._current_user.email
+        #self.current_user = Authentication.get_current_user()
+        self.current_user_email = 'created@example.com'
+        #self.load_tasks(current_user.email)
 
-        current_date_time = QDateTime.currentDateTime()
-        formatted_date = current_date_time.toString("dd-MM-yyyy")
-        self.teacher_main_name.setText(f"Welcome {User._current_user.name}")
-        self.teacher_main_date.setText(f"{formatted_date}")
-        
         #hide admin tab if user is not admin
         tab_widget = self.tabWidget
         if User._current_user.user_type != "admin":
@@ -50,8 +46,6 @@ class Main_Window(QMainWindow, Ui_MainWindow):
         #signal to create task button
         self.create_task_button.clicked.connect(self.create_task)
 
-        self.create_announcement_button.clicked.connect(self.create_announcement)
-
         self.show_Lesson_Schedule()
         self.show_Mentor_Schedule()
 
@@ -60,25 +54,26 @@ class Main_Window(QMainWindow, Ui_MainWindow):
         self.update_lessons.clicked.connect(self.refresh_lesson)
         self.update_mentoring.clicked.connect(self.refresh_mentor)
 
-
         self.lesson_att_insert.clicked.connect(self.show_lesson_attendance_page)
         self.mentor_att_insert.clicked.connect(self.show_mentor_attendance_page)
         self.lesson_att_show.clicked.connect(self.show_lesson_attendance_show)
         self.mentor_att_show.clicked.connect(self.show_mentor_attendance_show)
-        
+
         self.create_announcement_button.clicked.connect(self.create_announcement)
         self.delete_announcement_button.clicked.connect(self.delete_announcement)
-        self.update_information_button.clicked.connect(self.update_information)
+        #self.teacher_profil_city_edit.textChanged.connect(self.on_city_changed)
+        #self.teacher_profil_tel_edit.textChanged.connect(self.on_tel_changed)
 
-        print("teacher init is completed")
-        print("QTextBrowser Size:", self.announcement_textbrowser.toPlainText())
-
-    def update_information(self):
-        new_tel = self.teacher_profil_tel_edit.toPlainText()
+    def on_city_changed(self):
         new_city = self.teacher_profil_city_edit.toPlainText()
-        updated_info = {"phone_number": new_tel, "city": new_city  }
+        User.update_user_information(User._current_user.email, city=new_city)
+        self.showUpdateAlert("City is updated")
+
+    def on_tel_changed(self):
+        new_tel = self.teacher_profil_tel_edit.text()
+        updated_info = {"phone_number": new_tel }
         User.update_user_information(User._current_user.email, **updated_info)
-        self.showUpdateAlert("Information is updated")
+        self.showUpdateAlert("Phone number is updated")
 
     def display_announcement_to_delete(self):
         user = User._current_user
@@ -122,22 +117,17 @@ class Main_Window(QMainWindow, Ui_MainWindow):
             else:
                 self.showUpdateAlert(f"{message}")
         
-    def create_Teacher(self,name, surname, email, birthday, city,phone_number, password):
-        if not User.email_exists(email):
-            User.create_user(name, surname, email, birthday, city, phone_number, password, user_type="teacher")
-            print("User created successfully.")
         else:
-            QMessageBox.warning(None, 'Warning', f'The email {email} already exists.', QMessageBox.Ok)
+            self.showUpdateAlert(f"Announcement text cannot be empty!")
+        
+        
+    def display_announcements(self):
+        # Get announcements
+        announcements = User.get_announcements()
 
-    def is_valid_email(self, email):
-        email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-        return re.match(email_regex, email)
-    
-    def is_valid_password(self,password):
-        password = self.teacher_password_admin.text()
-
-        if len(password) <= 8 and any(c.isalpha() for c in password) and any(c.isdigit() for c in password) and any(c.isascii() and not c.isalnum() for c in password):
-            return True
+        if announcements is None or not announcements:
+            print("No announcement found.")
+            formatted_announcements = "No announcement"
         else:
             # Format announcements with gaps
             formatted_announcements = "<hr>".join(
@@ -147,13 +137,7 @@ class Main_Window(QMainWindow, Ui_MainWindow):
     )
 
         # Set the formatted text in the QTextBrowser
-        try:
-            # Set the formatted text in the QTextBrowser
-            self.announcement_textbrowser.setHtml(formatted_announcements)
-        except Exception as e:
-            print(f"Exception during setHtml: {e}")
-        print("Announcements are loaded")
-        print(formatted_announcements)
+        self.announcement_textbrowser.setHtml(formatted_announcements)
 
     def open_create_lesson(self):
 
